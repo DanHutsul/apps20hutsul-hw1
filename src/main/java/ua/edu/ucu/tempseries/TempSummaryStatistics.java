79d9203086ffdb0f4343d916ecb4c727e689fa3f
DanHutsul <dan.hutsul@gmail.com>
2020-11-13 19:05:22 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,8 +1,31 @@
 package main.java.ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    double avgTemp;
-    double devTemp;
-    double minTemp;
-    double maxTemp;
+    private double avgTemp;
+    private double devTemp;
+    private double minTemp;
+    private double maxTemp;
+    
+    TempSummaryStatistics(TemperatureSeriesAnalysis analysis) {
+        avgTemp = analysis.average();
+        devTemp = analysis.deviation();
+        minTemp = analysis.min();
+        maxTemp = analysis.max();
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
 }
